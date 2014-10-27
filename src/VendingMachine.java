4772846861f132496e472ab6628da66b77f258ed
Joe Longobardi <joe5656123@aol.com>
2014-10-27 12:28:28 -0400

    Merged Main
    Merged the main method VendingMachine.java

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -2,10 +2,12 @@
  * Programmer Names: Joe, Frank, and Keith
  * Class: IST 311
  * Project: Vending Machine Project
+ * Description: This program will simulate a Java Vending Machine JVM, 
+ * that will allow the user to interact with the machine with GUI
  */
 public class VendingMachine {
     public static void main(String[] args) {
-        // TODO code application logic here
+        Display.display();
     }
     
 }
